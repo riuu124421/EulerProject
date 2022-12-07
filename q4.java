7e98ede151667dfda1a961cb3b3ad5993be15d29
riya <79600393+riuu124421@users.noreply.github.com>
2022-12-07 20:13:38 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,23 @@
+package projecteuler;
+import java.util.*;
+public class q4 {
+public static boolean pali(long num) {
+	String x=num+"";
+	StringBuilder sb =new StringBuilder(x);
+	String st=sb.reverse().toString();
+	if(x.equals(st))
+		return true;
+	return false;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long max=Long.MIN_VALUE;
+		for(int i=100;i<=999;i++) {
+			for(int j=100;j<=999;j++) {
+				max=Math.max(max, i*j);
+			}
+		}
+
+	}
+System.out.println(max);
+}
