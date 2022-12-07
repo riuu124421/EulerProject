91b594338f0f863d3afd2df14e7a2f3c27a0d6c4
riya <79600393+riuu124421@users.noreply.github.com>
2022-12-07 20:05:06 +0530

    Add files via upload

---
+++ q9.java
@@ -0,0 +1,18 @@
+package projecteuler;
+
+public class q9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=1000;
+for(int i=1;i<=n/3;i++) {
+	for(int j =i+1;j<=n/2;j++) {
+		int k=n-i-j;
+		if(i*i+j*j==k*k) {
+			System.out.println(i*j*k);
+		}
+	}
+}
+	}
+
+}
